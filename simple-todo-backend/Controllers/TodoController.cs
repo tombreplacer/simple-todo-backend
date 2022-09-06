66f2c2aa769c063929ef184185c1bec22171f34d
brickstealer <wizzard344@gmail.com>
2022-09-06 19:24:41 +0300

    fff

--- simple-todo-backend/Controllers/TodoController.cs
+++ simple-todo-backend/Controllers/TodoController.cs
@@ -9,6 +9,8 @@ namespace simple_todo_backend.Controllers
         private readonly ILogger<TodoController> _logger;
         private readonly TodoContext _db;
 
+        private readonly Random _rng = new Random();
+
         public TodoController(ILogger<TodoController> logger, TodoContext db)
         {
             _logger = logger;
@@ -18,18 +20,24 @@ namespace simple_todo_backend.Controllers
         [HttpGet("")]
         public IEnumerable<Todo> Get()
         {
+            Thread.Sleep(_rng.Next(1,3000));
             return _db.Todos.ToArray();
         }
 
         [HttpGet("{id}")]
         public Todo Get(int id)
         {
+            Thread.Sleep(_rng.Next(1,3000));
             return _db.Todos.Where(e=>e.Id == id).First();
         }
 
         [HttpPost()]
         public void Add(Todo todo)
         {
+            Thread.Sleep(_rng.Next(1000,5000));
+            if(todo.Title == "error") {
+                throw new NotImplementedException();
+            }
             _db.Todos.Add(todo);
             _db.SaveChanges();
         }
@@ -37,6 +45,7 @@ namespace simple_todo_backend.Controllers
         [HttpPut()]
         public void Update(Todo todo)
         {
+            Thread.Sleep(_rng.Next(1000,5000));
             _db.Todos.Update(todo);
             _db.SaveChanges();
         }
@@ -44,7 +53,11 @@ namespace simple_todo_backend.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            Thread.Sleep(_rng.Next(1000,5000));
             var todo = _db.Todos.Where(e => e.Id == id).First();
+            if(todo.Title == "error") {
+                throw new NotImplementedException();
+            }
             _db.Todos.Remove(todo);
             _db.SaveChanges();
         }
